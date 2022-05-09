e18f1780ebe074810f9885b3a6d46d7243932e1b
BUKODONOZOR <90481288+BUKODONOZOR@users.noreply.github.com>
2022-05-09 02:59:28 -0500

    Add files via upload

---
+++ Front/src/App/AppUI.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
+
+function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+  
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+        
+   
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
